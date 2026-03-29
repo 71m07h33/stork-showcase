69b77515bc0c59d0c296f9c75ecd5fb036980c4f
71m07h33 <timothee.dumas.pro@gmail.com>
2026-03-29 08:23:59 +0200

    Removed un-necessary import

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Location, useLocation } from "../../app/providers/locationProvider";
 import { HomeHeader } from "./header";
 import { HomeWhom } from "./whom";
